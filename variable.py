bb8244eb904c2cf5d73134062506e52dd35a36b3
megha03-coder <meghachauhan310@gmail.com>
2025-09-13 14:17:05 +0530

    defining variable and print them

---
+++ variable.py
@@ -0,0 +1,13 @@
+# defining variable
+
+name="megha"
+age=22
+address="baghpat"
+
+print(name , age , address)       #print all the variables in the single line
+
+
+# to print all the variable in next line write different print statements
+print(name)
+print(age)
+print(address)
