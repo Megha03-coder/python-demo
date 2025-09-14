1b6236d373da36782bcdfd51f9666330e2a3fbba
megha03-coder <meghachauhan310@gmail.com>
2025-09-14 22:09:42 +0530

    string and its methods

---
+++ string.py
@@ -0,0 +1,19 @@
+# string is a Datatype - sequence of characters
+
+str="megha chauhan"
+print(str)
+print(str[3])        #print character of index 3 
+
+
+print(str[3:6])     # print characters from index 3 to 5 
+print(str[:9])       # starting index by default include
+
+
+# Methods of string
+
+print(str.endswith("a"))
+print(str.capitalize())
+
+print(str.replace("chauhan","singh"))
+print(str.find("h"))
+print(str.count("chauhan"))
