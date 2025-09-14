d8150636900a1cbfe862fbf9c74b8f3f0e1b4cd4
megha03-coder <meghachauhan310@gmail.com>
2025-09-14 22:22:40 +0530

    elif-statements

---
+++ elif.py
@@ -0,0 +1,10 @@
+# multiple conditions
+
+
+age=int (input("enter your age"))
+if(age>18):
+    print("you can drive")
+elif(age>22):
+    print("you can vote")       # exceute when the if conditions is false
+else:
+    print("you can not vote")        
