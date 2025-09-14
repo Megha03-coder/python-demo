5cc69ccdab4e3782afb6857a7fd6281f00ed13ad
megha03-coder <meghachauhan310@gmail.com>
2025-09-14 21:50:48 +0530

    add comments and type cast the input

--- inputFromUser.py
+++ inputFromUser.py
@@ -1,7 +1,7 @@
-name = input("enter your name")
+name = input("enter your name")       # input function always return <str>
 print(name)
 print(type(name))
 
-Age=  int(input("enter your age")) 
+Age=  int(input("enter your age"))    # we have use type casting to convert the return string to integer 
 print(Age)
 print(type(Age))
