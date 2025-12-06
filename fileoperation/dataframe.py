2c3fa1b3f08fda1c98ff5498f619cba15d897f09
Megha03-coder <meghachauhan310@gmail.com>
2025-12-06 22:19:07 +0530

    load files into Data Frame

--- fileoperation/dataframe.py
+++ fileoperation/dataframe.py
@@ -1,3 +1,3 @@
 import pandas as pd
-df=pd.read_csv("customers-100.csv")
-print(df)
+df = pd.read_csv(r"C:\Users\tarun\python-demo\fileoperation\customers-100.csv")
+print(df.to_string())       # print(df) only starting 5 rows/5 last rows
