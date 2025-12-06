63df16da671f8f38580273c828c48842dc7b03ee
Megha03-coder <meghachauhan310@gmail.com>
2025-12-06 22:28:13 +0530

    operaions in 1-D array (numpy)

---
+++ numpyoperation/operationson1-D.py
@@ -0,0 +1,12 @@
+import numpy as np
+list1=[[10,20,50,30,40,5,60],[80,60,40,10,30,20,4]]
+array1=np.array(list1)
+print(array1.max())
+print(array1.min())
+print(array1.sum())
+print(array1.mean())
+print(array1.prod())
+print(array1.var())
+print(array1.std())
+
+
