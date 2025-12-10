af2f434d9293e9c899adda23b597f5ebced932a7
Megha03-coder <meghachauhan310@gmail.com>
2025-12-10 19:41:52 +0530

    opearions on table

--- fileoperation/openFile.py
+++ fileoperation/openFile.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 f=open("C:/Users/tarun/python-demo/fileoperation/customers-100.csv","r")
+
 print(f.read())
 
 
--- numpyoperation/dataframe.py
+++ numpyoperation/dataframe.py
@@ -2,3 +2,9 @@ import pandas as pd
 std_data=[(1,'megha','sisana'),(2,'rakhi','baghpat')]
 df=pd.DataFrame(std_data,columns=['ID','name','address'])
 print(df)
+
+#operations on table
+print(df['name'])     # print cloumns
+
+print(df.loc[1])      #print row with the specified index
+
