97a184dfe93ed6ea008b48ecff5a49b400aa8aa3
Megha03-coder <meghachauhan310@gmail.com>
2025-12-10 20:16:43 +0530

    scatter and line both

---
+++ matplotlib/scatter.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+x_values=[1,2,3,4,5]
+y_values=[2,4,6,8,10]
+plt.scatter(x_values,y_values)
+
+other_valuesx=[1,2,3,4,5]
+other_valuesy=[2,4,6,8,10]
+plt.plot(other_valuesx,other_valuesy)
+plt.show()
